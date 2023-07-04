34dfa41864615bd55c8025e8ac9ce784e5b004cc
Davidguzley <58478139+Davidguzley@users.noreply.github.com>
2023-07-04 14:50:55 -0600

    style: Add function descriptions

--- Proyect/server/controllers/productController.js
+++ Proyect/server/controllers/productController.js
@@ -1,19 +1,26 @@
+const Product = require('../models/productModel');
+
+// Create new workout
 const createProduct = async (req, res) => {
     res.json({mssg: 'createProduct'})
 };
 
+// Get all products
 const getProducts = async (req, res) => {
     res.json({mssg: 'getProducts'})
 };
 
+// Get a single product
 const getProduct = async (req, res) => {
     res.json({mssg: 'getProduct'})
 };
 
+// Delete a product
 const deleteProduct = async (req, res) => {
     res.json({mssg: 'deleteProduct'})
 };
 
+// Update a product
 const updateProduct = async (req, res) => {
     res.json({mssg: 'updateProduct'})
 };
