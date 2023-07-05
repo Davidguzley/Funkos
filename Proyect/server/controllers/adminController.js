e311af52297d2ec655b8f7703ee093688b3f57fa
Davidguzley <58478139+Davidguzley@users.noreply.github.com>
2023-07-05 09:16:50 -0600

    feat: Update admin user from API

--- Proyect/server/controllers/adminController.js
+++ Proyect/server/controllers/adminController.js
@@ -1,5 +1,5 @@
 const Admin = require('../models/adminModel');
-const jwt = require ('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 const mongoose = require('mongoose');
 
 const createToken = (_id) => {
@@ -8,49 +8,87 @@ const createToken = (_id) => {
 
 // Create new Admin user
 const createAdmin = async (req, res) => {
-    const {first_name, last_name, email, password} = req.body
+    const { first_name, last_name, email, password } = req.body;
 
     // signup Admin
     try {
         const admin = await Admin.signup(first_name, last_name, email, password);
-        
+
         // create a token for admin
-        const token = createToken(admin._id)
-        
-        res.status(200).json({email, token});
+        const token = createToken(admin._id);
+
+        res.status(200).json({ email, token });
     } catch (error) {
-        res.status(400).json({error: error.message});
+        res.status(400).json({ error: error.message });
     }
 };
 
 const getAdmins = async (req, res) => {
-    const admins = await Admin.find({}).sort({createdAt: -1});
+    const admins = await Admin.find({}).sort({ createdAt: -1 });
 
     res.status(200).json(admins);
 };
 
 const getAdmin = async (req, res) => {
-    res.json({mssg: 'getAdmin'})
+    res.json({ mssg: 'getAdmin' });
 };
 
 const deleteAdmin = async (req, res) => {
     const { id } = req.params;
 
     if (!mongoose.Types.ObjectId.isValid(id)) {
-        return res.status(404).json({error: 'No such admin'});
+        return res.status(404).json({ error: 'No such admin' });
     }
 
-    const admin = await Admin.findOneAndDelete({_id: id});
+    const admin = await Admin.findOneAndDelete({ _id: id });
 
     if (!admin) {
-        return res.status(404).json({error: 'No such admin'});
+        return res.status(404).json({ error: 'No such admin' });
     }
 
     res.status(200).json(admin);
 };
 
 const updateAdmin = async (req, res) => {
-    res.json({mssg: 'updateAdmin'})
+    const { id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ error: 'No such admin' });
+    }
+
+    const { first_name, last_name, email } = req.body;
+
+    try {
+        let admin = await Admin.findById(id);
+
+        if (!admin) {
+            return res.status(404).json({ error: 'No such admin' });
+        }
+
+        // Check if any field is empty
+        if (!first_name || !last_name || !email) {
+            return res.status(400).json({ error: 'All fields must be filled' });
+        }
+
+        // Check if the email is being changed
+        if (email !== admin.email) {
+            const emailExists = await Admin.findOne({ email });
+            if (emailExists) {
+                return res.status(400).json({ error: 'Email already in use' });
+            }
+        }
+
+        // Update admin attributes
+        admin.first_name = first_name;
+        admin.last_name = last_name;
+        admin.email = email;
+
+        const updatedAdmin = await admin.save();
+
+        res.status(200).json(updatedAdmin);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
 };
 
 module.exports = {
@@ -58,5 +96,5 @@ module.exports = {
     getAdmins,
     getAdmin,
     deleteAdmin,
-    updateAdmin
+    updateAdmin,
 };
